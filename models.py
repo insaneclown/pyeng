349541017974218e0278fe98d7fc3c3133bc2e06
Insane Clown <insaneclown42@gmail.com>
2013-01-09 15:11:24 +0100

    Create user model.

--- models.py
+++ models.py
@@ -10,3 +10,14 @@ class BaseModel(peewee.Model):
     """
     class Meta:
         database = database
+
+
+class User(BaseModel):
+    """Representation of the user model.
+
+    Because the applications are to be stored for many users statistics
+    therefore there is a need to have a User model.
+
+    User has only its own unique name.
+    """
+    name = peewee.CharField(unique=True)
