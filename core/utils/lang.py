6bbc0ab029b45d98143003c35ad9ff68f8e68d6d
Insane Clown <insaneclown42@gmail.com>
2013-01-10 16:24:37 +0100

    Implement is_package.

--- core/utils/lang.py
+++ core/utils/lang.py
@@ -1,16 +1,17 @@
+from os import path
 
 
-def is_package(path):
+def is_package(dir):
     """Checks if the specified path is a package.
 
     Checks whether a given path is a directory
     that contains the file __init__.py.
 
     Args:
-        path:
-            Specified path.
+        dir:
+            Directory path specified package.
 
     Returns:
         True if it's a package, otherwise False.
     """
-    pass
+    return path.isdir(dir) and path.isfile(path.join(dir, '__init__.py'))
