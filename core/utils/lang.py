344466fe1b5327fd1f8aa029d55012a85c0ff582
Insane Clown <insaneclown42@gmail.com>
2013-01-10 03:24:50 +0100

    Add empty function 'is_package'.

---
+++ core/utils/lang.py
@@ -0,0 +1,16 @@
+
+
+def is_package(path):
+    """Checks if the specified path is a package.
+
+    Checks whether a given path is a directory
+    that contains the file __init__.py.
+
+    Args:
+        path:
+            Specified path.
+
+    Returns:
+        True if it's a package, otherwise False.
+    """
+    pass
