b909fd7f2d654670001d50748dc793c09c8ffecc
Insane Clown <insaneclown42@gmail.com>
2013-01-11 14:47:29 +0100

    Implement functions to sync database and fix test.

--- core/db.py
+++ core/db.py
@@ -3,12 +3,17 @@
 Module with the tools to work with the database.
 """
 
+from core.models import User
+from plugins.sample.models import Sample
+
+
 def sync_database():
     """Synchronizes the database.
 
     Creates the database tables for core and all plugins
     whose tables have not already been created.
     """
+    create_tables(get_model_classes())
 
 
 def get_model_classes():
@@ -17,6 +22,9 @@ def get_model_classes():
     Returns:
         List of model based on BaseModel.
     """
+    # TODO: add automatic models detection from plugins
+    return [User, Sample]
+
 
 def create_tables(model_classes):
     """Creates the database tables for given model classes
@@ -26,4 +34,5 @@ def create_tables(model_classes):
         model_classes:
             List of model based on BaseModel.
     """
-
+    for model_class in model_classes:
+        model_class.create_table(fail_silently=True)
--- tests/core/db/create_tables_tests.py
+++ tests/core/db/create_tables_tests.py
@@ -7,5 +7,5 @@ def test_when_given_few_model_classes():
     first_class = Mock()
     second_class = Mock()
     create_tables([first_class, second_class])
-    first_class.assert_called_with(fail_silently=True)
-    second_class.assert_called_with(fail_silently=True)
+    first_class.create_table.assert_called_with(fail_silently=True)
+    second_class.create_table.assert_called_with(fail_silently=True)
